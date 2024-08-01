43f57e0ef59715fd5d0feb2c5525f75685e33cf8
Gabriel Pereira <125773807+gabrielpereir@users.noreply.github.com>
2024-08-01 16:02:20 -0300

    Delete Javascript directory

--- Javascript/Capturando dados/script.js
+++
@@ -1,5 +0,0 @@
-window.alert('Bem vindo!\nA seguir pediremos que informe alguns dados');
-let nome = window.prompt('Digite seu nome: ');
-let idade = parseInt(window.prompt('Digite sua idade'));
-confirm('confirma sua idade.');
-alert('Nome: '+ nome +'\nIdade: '+ idade);
